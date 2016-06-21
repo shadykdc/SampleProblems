3fc29b782076ad45216c6222f8f84fee68e384d8
kshea <kathryn.shea@gmail.com>
2016-06-21 14:29:15 +0100

    Update parser.cpp

--- parser.cpp
+++ parser.cpp
@@ -6,32 +6,28 @@ using namespace std;
 vector<vector<string>> csv_parser(string input, const char &row_delim, const char &col_delim, const char &q){
     vector<vector<string>> output;
     bool quote = false;
-    string str;
+    string one_string;
     vector<string> one_row;
     
     for (char &c : input){
         if (c == q){
             quote = !quote;
-            continue;
         }
-        if (quote){
-            str += c;
+        else if (quote || (c != col_delim && c != row_delim)){
+            one_string += c;
         }
         else if (c == col_delim){
-            one_row.push_back(str);
-            str.clear();
+            one_row.push_back(one_string);
+            one_string.clear();
         }
-        else if (c == row_delim){
-            one_row.push_back(str);
+        else if (c == row_delim){ // could've used "else" but I'm being explicit
+            one_row.push_back(one_string);
             output.push_back(one_row);
-            str.clear();
+            one_string.clear();
             one_row.clear();
         }
-        else {
-            str += c;
-        }
     }
-    one_row.push_back(str);
+    one_row.push_back(one_string);
     output.push_back(one_row);
     
     return output;
@@ -44,9 +40,7 @@ int main(){
     const char &col = ',';
     const char &q = '"';
     
-    vector<vector<string>> output;
-    
-    output = csv_parser(in, row, col, q);
+    vector<vector<string>> output = csv_parser(in, row, col, q);
     
     cout << "[" << endl;
     for (int i = 0; i < output.size(); i++){
